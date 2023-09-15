bb55a6b66dce4cbdaaee7e4ecee8f65c9fb98aa3
ProfL3304 <ProfL3304@lab.aca>
2023-09-15 16:18:51 -0500

    Movimiento horizontal con flip

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,10 +11,12 @@ public class PlayerMovement : MonoBehaviour
 
     private float moveDirection = 0f;
     private Rigidbody2D rb;
+    private Animator animator;
 
     private void Start() 
     {
         rb = GetComponent<Rigidbody2D>();   
+        animator = GetComponent<Animator>();
     }
 
     private void OnMove(InputValue value)
@@ -22,12 +24,38 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = value.Get<float>();
     }
 
-    private void Update() 
+    private void Update()
     {
+        Run();
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        if (Mathf.Abs(rb.velocity.x) > Mathf.Epsilon)
+        {
+            transform.localScale = new Vector3(
+                Mathf.Sign(rb.velocity.x),
+                1f,
+                1f
+            );
+        }
+    }
+
+    private void Run()
+    {
+        if (moveDirection == 0f)
+        {
+            animator.SetBool("IsRunning", false);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", true);
+        }
+
         rb.velocity = new Vector2(
             runSpeed * moveDirection,
             rb.velocity.y
         );
     }
-
 }
